f42e1d815b6615e96b17a777e91a501336863c62
elijahcbrooks <elijahcbrooks@gmail.com>
2021-04-08 18:38:06 -0500

    inital commit

--- Assignment_DayThree/src/AssignmentOneDayThree.java
+++ Assignment_DayThree/src/AssignmentOneDayThree.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
  */
 public class AssignmentOneDayThree {
     /**
-     *
      * @param args
      */
     public static void main(String[] args) {
@@ -17,21 +16,20 @@ public class AssignmentOneDayThree {
         try {
             File directory = new File(scan.nextLine().trim());
             newObject.parseDirectory(directory);
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Invalid directory.");
         }
 
     }
 
     /**
-     *
      * @param directory File object made from user input. Assumed to be a directory
      */
-    public void parseDirectory(File directory){
+    public void parseDirectory(File directory) {
         File[] fileList = directory.listFiles();
-        for (File file : fileList){
+        for (File file : fileList) {
             System.out.println(file);
-            if(file.listFiles() != null)
+            if (file.listFiles() != null)
                 parseDirectory(file);
         }
     }
