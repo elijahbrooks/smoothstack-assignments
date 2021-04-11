c66dfb767f06720d02ae57978fc5554ce3d59551
elijahcbrooks <elijahcbrooks@gmail.com>
2021-04-11 00:12:17 -0500

    comments added

--- Assignment_DayFive/src/AssignmentThreeDayFive.java
+++ Assignment_DayFive/src/AssignmentThreeDayFive.java
@@ -2,7 +2,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * @author elijahbrooks
+ */
 public class AssignmentThreeDayFive {
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         List<String> listOfStrings = Arrays.asList("Elijah", "amm", "Is", "A", "Goat", "app");
 
