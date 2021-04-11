bfe79e82feeae1747e2f76569ea16ce4ca0a256c
elijahcbrooks <elijahcbrooks@gmail.com>
2021-04-11 00:11:26 -0500

    Assignment three completed

---
+++ Assignment_DayFive/src/AssignmentThreeDayFive.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AssignmentThreeDayFive {
+    public static void main(String[] args) {
+        List<String> listOfStrings = Arrays.asList("Elijah", "amm", "Is", "A", "Goat", "app");
+
+        List<String> listOfStringsWithA = listOfStrings.stream()
+                .filter((string) ->
+                        string.length() == 3 && (string.contains("a") || string.contains("A"))
+                )
+                .collect(Collectors.toList());
+
+        for(String string : listOfStringsWithA)
+            System.out.println(string);
+
+    }
+}
